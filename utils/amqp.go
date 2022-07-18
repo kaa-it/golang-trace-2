a30e51ea59a2b07fa8e013788321fedc2985f1d5
Andrey Kruglov <kruglov.it@gmail.com>
2022-07-18 20:32:04 +0300

    utils with amqp and carriers

---
+++ utils/amqp.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func ConnectAmqp(user, pass, host, port string) (*ampq.Channel, func() error) {
+	address := fmt.Sprintf("amqp://%s:%s@%s:%s/", user, pass, host, port)
+
+	connection, err := amqp.Dial(address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	channel, err := connection.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = channel.ExchangeDeclare("exchange", "direct", true, false, false, false, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return channel, connection.Close
+}
